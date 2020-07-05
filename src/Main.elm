eeb29c073fc3cbe10a48cc8dce35221b63135cf8
Alex <alex@42layers.io>
2020-07-05 08:54:09 -0700

    JS interop

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,75 @@
-module Main exposing (..) 
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (Attribute, Html, div, input, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
+
+
 -- MAIN
+
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
+
+-- PORTS
+
+
+port sendMessage : String -> Cmd msg
+port messageReceiver : (String -> msg) -> Sub msg
+
+
 
 -- MODEL
+
+
 type alias Model =
-  { content : String
-  }
-init : Model
-init =
-  { content = "" }
+    { content : String
+    , xml : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( { content = ""
+      , xml = "init value"
+      }
+    , Cmd.none
+    )
+
+
 
 -- UPDATE
+
+
 type Msg
-  = Change String
-update : Msg -> Model -> Model
+    = Compile String
+    | Recv String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    Change newContent ->
-      { model | content = newContent }
+    case msg of
+        Compile newSource ->
+            ({ model | content = newSource }, sendMessage model.content)
+
+        Recv newXML ->
+            ({ model | xml = newXML }, Cmd.none)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  messageReceiver Recv
 
 -- VIEW
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
-    , div [] [ text ("This will eventually be the compiled XML") ]
-    ]
+    div []
+        [ input [ placeholder "Text to reverse", value model.content, onInput Compile ] []
+        , div [] [ text model.xml ]
+        ]
