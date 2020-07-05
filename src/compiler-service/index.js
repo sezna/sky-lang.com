9b46d929486eb96edb30b1bac75b092b80e0fb46
Alex <alex@42layers.io>
2020-07-05 10:20:55 -0700

    working compile service

--- src/compiler-service/index.js
+++ src/compiler-service/index.js
@@ -6,21 +6,23 @@ const PORT = 1414;
 function handleRequest(request, response) {
   switch (request.method) {
     case "POST":
-      if (request.body && request.body.sourceCode) {
-        let result = sky(request.body.sourceCode);
+      let body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log("requested code: ", body);
+        // at this point, `body` has the entire request body stored in it as a string
+        let result = sky(body);
         response.writeHead(200, { "Content-Type": "text/html" });
         response.end(JSON.stringify(result));
         console.log("responded with compiled message");
-      } else {
-        response.writeHead(400, { "Content-Type": "text/html" });
-        response.end("Malformed request");
-        console.log("bad request");
-      }
+      });
     break;
   default:
       response.writeHead(400, { "Content-Type": "text/html" });
       response.end("Malformed request");
-      console.log("bad request");
+      console.log("bad request: method not supported");
   }
 }
 
