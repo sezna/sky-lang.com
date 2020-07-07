b3cc2fe818d233dd85a90df39c0a0901ffae3e4e
Alex <alex@42layers.io>
2020-07-07 09:03:41 -0700

    add morgan logging

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require('morgan')
 
 
 
 const PORT = 1414;
 
 let app = express();
+app.use(morgan('combined'))
 app.use(bodyParser.text());
 
 app.post("/api/compile", (request, response) => {
-        console.log("requested code: ", request.body);
+        console.log("   ", request.body);
         let result = sky(request.body);
         response.json(result);
-        console.log("responded with compiled message");
       });
 
 app.get("/", (_, response) => {
